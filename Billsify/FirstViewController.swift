6821405438afd475732c4b55e9aa73572a81e578
Lisbet Meneses <lis.mene.alo@gmail.com>
2019-11-10 12:58:32 -0800

    Initial Commit

---
+++ Billsify/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Billsify
+//
+//  Created by Lisbet Meneses on 10/11/2019.
+//  Copyright © 2019 Lisbet Meneses. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Billsify/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Billsify
+//
+//  Created by Lisbet Meneses on 10/11/2019.
+//  Copyright © 2019 Lisbet Meneses. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
