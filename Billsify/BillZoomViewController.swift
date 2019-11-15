707211a273dc4c906bbd17a21b3b3df580b8edbc
Lisbet Meneses <lis.mene.alo@gmail.com>
2019-11-15 13:50:09 -0800

    Working on gallery

---
+++ Billsify/BillZoomViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BillZoomViewController.swift
+//  Billsify
+//
+//  Created by Lisbet Meneses on 15/11/2019.
+//  Copyright © 2019 Lisbet Meneses. All rights reserved.
+//
+
+import UIKit
+
+class BillZoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
