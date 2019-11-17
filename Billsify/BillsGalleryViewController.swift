b30190f4051ecb09914fda29d3f3654ab9a81eb1
Lisbet Meneses <lis.mene.alo@gmail.com>
2019-11-17 11:18:22 -0800

    Working on gallery

--- Billsify/BillsGalleryViewController.swift
+++ Billsify/BillsGalleryViewController.swift
@@ -90,4 +90,28 @@ class BillsGalleryViewController: UIViewController {
         CategoryDetailsViewController.category = subview.text!
         performSegue(withIdentifier: "categoryDetails", sender: self)
     }
+    
+
+    func textToImage(drawText text: String, inImage image: UIImage, atPoint point: CGPoint) -> UIImage {
+       let textColor = UIColor.white
+       let textFont = UIFont(name: "Helvetica Bold", size: 12)!
+
+       let scale = UIScreen.main.scale
+       UIGraphicsBeginImageContextWithOptions(image.size, false, scale)
+
+       let textFontAttributes = [
+           NSAttributedStringKey.font: textFont,
+           NSAttributedStringKey.foregroundColor: textColor,
+           ] as [NSAttributedStringKey : Any]
+       image.draw(in: CGRect(origin: CGPoint.zero, size: image.size))
+
+       let rect = CGRect(origin: point, size: image.size)
+       text.draw(in: rect, withAttributes: textFontAttributes)
+
+       let newImage = UIGraphicsGetImageFromCurrentImageContext()
+       UIGraphicsEndImageContext()
+
+       return newImage!
+    }
+    
 }
